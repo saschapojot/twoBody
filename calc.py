bf568cb5c1f9c9ed71a6e19e4b3f3264c1fbbc99
Xi LIU <e0385051@u.nus.edu>
2021-08-25 22:03:59 +0800

    changed one line

--- calc.py
+++ calc.py
@@ -1,10 +1,11 @@
 from funcs import *
 
-outDir="./pump1"
+outDir="./pump1/"
 tStart=datetime.now()
 dataAll=[]# list of all wvfcnts
 dataAll.append(psi0)
 for q in range(0,Q):
+    print("q = "+str(q))
     psiCurr=dataAll[q]
     psiNext=S2(q,psiCurr)
     psiNext=reNormalization(psiNext)
@@ -18,15 +19,15 @@ for q in range(0,Q+1):
     xTmp=avgPos(vecTmp)
     xPosAll.append(xTmp)
 
-drift=[elem -xPosAll[0] for elem in xPosAll]
-driftRealPart=[np.real(elem) for elem in drift]
-driftImagPart=[np.imag(elem) for elem in drift]
-print("max imag = "+np.max(np.abs(driftImagPart)))
+drift=[np.real(elem -xPosAll[0]) for elem in xPosAll]
+
+driftImagPart=[np.imag(elem-xPosAll[0]) for elem in xPosAll]
+print(np.max(np.abs(driftImagPart)))
 tAll=[dt*q for q in range(0,Q+1)]
 plt.figure(figsize=(20,20))
 plt.plot(tAll,drift,color="black")
 plt.xlabel("time")
 plt.ylabel("ave position")
-plt.title("initial position = "+str(L)+", pumping = "+str(driftRealPart[-1]-driftRealPart[0]))
+plt.title("initial position = "+str(L)+", pumping = "+str(drift[-1]-drift[0]))
 plt.savefig(outDir+"init"+str(L)+".png")
 plt.close()
